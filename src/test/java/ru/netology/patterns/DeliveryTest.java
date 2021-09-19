eb0ca64655bc9a35d36df2c6efbfd7106ca6370a
Ilya Filimonov <ilyafil1996@yandex.ru>
2021-09-19 19:05:57 +0300

    Add AllureReport

--- src/test/java/ru/netology/patterns/DeliveryTest.java
+++ src/test/java/ru/netology/patterns/DeliveryTest.java
@@ -1,9 +1,9 @@
 package ru.netology.patterns;
 
 import com.codeborne.selenide.Condition;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
 
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
@@ -13,9 +13,20 @@ import static ru.netology.patterns.DataGenerator.*;
 public class DeliveryTest {
 
     @BeforeEach
-    public void setUpAll() {
+    public void setUpAllure() {
         open("http://localhost:9999");
     }
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
 
     @Test
     @DisplayName("Should successfully login with active registered user")
